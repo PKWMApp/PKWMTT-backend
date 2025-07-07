c683980c9f89608ae8bd4a8d901cfb366cf68428
Mikołaj Florczak <84631301+florczaq@users.noreply.github.com>
2025-07-07 21:14:30 +0200

    Initialization

---
+++ src/main/java/org/pkwmtt/PkwmttBackendApplication.java
@@ -0,0 +1,13 @@
+package org.pkwmtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkwmttBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkwmttBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pkwmtt/pkwmttbackend/PkwmttBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pkwmtt.pkwmttbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkwmttBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
