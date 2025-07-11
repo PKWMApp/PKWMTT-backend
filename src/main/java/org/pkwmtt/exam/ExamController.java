f4e1844f282a5e3dbaa0d543d9b079071200debf
Patryk Mazurek <Patryk.m3456@gmail.com>
2025-07-11 19:03:20 +0200

    add javadocs to endpoints and small fix

--- src/main/java/org/pkwmtt/exam/ExamController.java
+++ src/main/java/org/pkwmtt/exam/ExamController.java
@@ -8,39 +8,67 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RequiredArgsConstructor
-@RequestMapping("/pkwmtt/api/v1")
+@RequestMapping("/pkwmtt/api/v1/exams")
 @RestController
 public class ExamController {
 
     private final ExamService examService;
 
-    @PostMapping("/exams")
-    public ResponseEntity<Void> addExam(@RequestBody ExamDto examDto){
+    /**
+     * @param exam details of exam
+     * @return
+     */
+    @PostMapping("")
+    public ResponseEntity<Void> addExam(@RequestBody ExamDto exam) {
         throw new UnsupportedOperationException("Not supported yet.");
 //        return ResponseEntity.created().build();
     }
 
-    @PutMapping("/exams")
-    public ResponseEntity<Void> modifyExam(@RequestBody ExamEntity exam){
+    /**
+     * @param id of exam or test
+     * @param exam new details of exam or test
+     * @return
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> modifyExam(@PathVariable long id, @RequestBody ExamEntity exam) {
         throw new UnsupportedOperationException("Not supported yet.");
 //        return ResponseEntity.noContent().build();
     }
 
-    @DeleteMapping("/exams/{id}")
-    public ResponseEntity<Void> deleteExam(@PathVariable long id){
+    /**
+     * @param id of exam or test
+     * @return
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteExam(@PathVariable long id) {
         throw new UnsupportedOperationException("Not supported yet.");
 //        return ResponseEntity.noContent().build();
     }
 
-    @GetMapping("/singleExam/{id}")
-    public ResponseEntity<ExamEntity> getExam(@PathVariable long id){
+    /**
+     * @param id of exam or test
+     * @return single exam or test details
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<ExamEntity> getExam(@PathVariable long id) {
         throw new UnsupportedOperationException("Not supported yet.");
 //        return ResponseEntity.ok();
     }
 
-//    groups format {12K2K04P04L04}
-    @GetMapping("/exams/{groups}")
-    public ResponseEntity<List<ExamEntity>> getExams(@PathVariable String groups){
+    /**
+     * @param generalGroup symbol that identify exercise group of specific field of study (for example 12K2)
+     * @param k computer laboratory group
+     * @param l laboratory group
+     * @param p project group
+     * @return
+     */
+    @GetMapping("/by-groups/{generalGroup}")
+    public ResponseEntity<List<ExamEntity>> getExams(
+            @PathVariable String generalGroup,
+            @RequestParam(name = "k", required = false) String k,
+            @RequestParam(name = "l", required = false) String l,
+            @RequestParam(name = "p", required = false) String p
+            ) {
         throw new UnsupportedOperationException("Not supported yet.");
 //        return ResponseEntity.ok();
     }
