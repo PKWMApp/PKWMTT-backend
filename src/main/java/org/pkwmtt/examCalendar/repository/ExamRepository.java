48a983da75b5aa5bf9483adedcf2ec719a31326b
PatMaz999 <patryk.m.3456@gmail.com>
2025-08-06 17:27:58 +0200

    implement integration tests for ExamRepository (with h2 database in test scope)

--- src/main/java/org/pkwmtt/examCalendar/repository/ExamRepository.java
+++ src/main/java/org/pkwmtt/examCalendar/repository/ExamRepository.java
@@ -11,6 +11,42 @@ public interface ExamRepository extends JpaRepository<Exam, Integer> {
     @Query("select e from Exam e where e.examGroups LIKE %:gs%")
     Set<Exam> findExamsByGroupSignature(@Param("gs")String groupSignature);
 
+    @Query("SELECT e FROM Exam e WHERE " +
+            "e.examGroups LIKE CONCAT('%', :g1, '%') OR " +
+            "e.examGroups LIKE CONCAT('%', :g2, '%') OR " +
+            "e.examGroups LIKE CONCAT('%', :g3, '%') OR " +
+            "e.examGroups LIKE CONCAT('%', :g4, '%') ")
+    Set<Exam> findExamsByGroupsIdentifier(
+            @Param("g1") String group1,
+            @Param("g2") String group2,
+            @Param("g3") String group3,
+            @Param("g4") String group4
+    );
+
+    @Query("SELECT e FROM Exam e WHERE " +
+            "e.examGroups LIKE CONCAT('%', :g1, '%') OR " +
+            "e.examGroups LIKE CONCAT('%', :g2, '%') OR " +
+            "e.examGroups LIKE CONCAT('%', :g3, '%') ")
+    Set<Exam> findExamsByGroupsIdentifier(
+            @Param("g1") String group1,
+            @Param("g2") String group2,
+            @Param("g3") String group3
+    );
+
+    @Query("SELECT e FROM Exam e WHERE " +
+            "e.examGroups LIKE CONCAT('%', :g1, '%') OR " +
+            "e.examGroups LIKE CONCAT('%', :g2, '%')" )
+    Set<Exam> findExamsByGroupsIdentifier(
+            @Param("g1") String group1,
+            @Param("g2") String group2
+    );
+
+    @Query("SELECT e FROM Exam e WHERE " +
+            "e.examGroups LIKE CONCAT('%', :gg, '%')")
+    Set<Exam> findExamsByGroupsIdentifier(
+            @Param("gg") String group
+    );
+
     /**
      * @param groupSignature symbol that identifies group
      * @return set of Exams for specific group
---
+++ src/test/java/org/pkwmtt/examCalendar/repository/ExamRepositoryTest.java
@@ -0,0 +1,182 @@
+package org.pkwmtt.examCalendar.repository;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.pkwmtt.examCalendar.entity.Exam;
+import org.pkwmtt.examCalendar.entity.ExamType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@DataJpaTest
+class ExamRepositoryTest {
+
+    @Autowired
+    private ExamRepository examRepository;
+
+    @Autowired
+    private ExamTypeRepository examTypeRepository;
+
+    private ExamType examType;
+
+    @BeforeEach
+    void setup(){
+        examType = ExamType.builder()
+                .name("exam")
+                .build();
+        examTypeRepository.save(examType);
+    }
+
+    /**
+     * test if method find specific count of exams when 1 or 0 group identifiers match
+     */
+    @Test
+    void testSingleIdentifierMatch() {
+//        given
+        Exam exam1 = Exam.builder()
+                .title("Exam 1")
+                .description("Exam 1")
+                .date(LocalDateTime.now().plusDays(1))
+                .examGroups("12K2, K03")
+                .examType(examType)
+                .build();
+        examRepository.save(exam1);
+        Exam exam2 = Exam.builder()
+                .title("Exam 2")
+                .description("Exam 2")
+                .date(LocalDateTime.now().plusDays(1))
+                .examGroups("12K3, K03, S02")
+                .examType(examType)
+                .build();
+        examRepository.save(exam2);
+        Exam exam3 = Exam.builder()
+                .title("Exam 3")
+                .description("Exam 3")
+                .date(LocalDateTime.now().plusDays(1))
+                .examGroups("13K1, K05, L05")
+                .examType(examType)
+                .build();
+        examRepository.save(exam3);
+        Exam exam4 = Exam.builder()
+                .title("Exam 4")
+                .description("Exam 4")
+                .date(LocalDateTime.now().plusDays(1))
+                .examGroups("41K1, L04, P03, I01")
+                .examType(examType)
+                .build();
+        examRepository.save(exam4);
+        Exam exam5 = Exam.builder()
+                .title("Exam 5")
+                .description("Exam 5")
+                .date(LocalDateTime.now().plusDays(1))
+                .examGroups("11A1, G03, H01, P02")
+                .examType(examType)
+                .build();
+        examRepository.save(exam5);
+
+        String generalGroup = "12K2";
+        String kGroup = "K05";
+        String lGroup = "L04";
+        String pGroup = "P02";
+
+//        when
+        Set<Exam> exams = examRepository.findExamsByGroupsIdentifier(generalGroup, kGroup, lGroup, pGroup);
+        List<String> examsTitles = exams.stream().map(Exam::getTitle).toList();
+//        then
+        assertEquals(4, exams.size());
+        assertTrue(examsTitles.contains("Exam 1"));
+        assertTrue(examsTitles.contains("Exam 3"));
+        assertTrue(examsTitles.contains("Exam 4"));
+        assertTrue(examsTitles.contains("Exam 5"));
+    }
+
+    /**
+     * test if method don't duplicate exams when more than 1 identifier match
+     */
+    @Test
+    void testMultipleIdentifierMatch() {
+//        given
+        Exam exam1 = Exam.builder()
+                .title("Exam 1")
+                .description("Exam 1")
+                .date(LocalDateTime.now().plusDays(1))
+                .examGroups("12K2, K01, L04, P03, I01")
+                .examType(examType)
+                .build();
+        examRepository.save(exam1);
+        Exam exam2 = Exam.builder()
+                .title("Exam 2")
+                .description("Exam 2")
+                .date(LocalDateTime.now().plusDays(1))
+                .examGroups("12K2, K05, L04, P02")
+                .examType(examType)
+                .build();
+        examRepository.save(exam2);
+        Exam exam3 = Exam.builder()
+                .title("Exam 3")
+                .description("Exam 3")
+                .date(LocalDateTime.now().plusDays(1))
+                .examGroups("12K2, K05, L04, P02, I05")
+                .examType(examType)
+                .build();
+        examRepository.save(exam3);
+
+        String generalGroup = "12K2";
+        String kGroup = "K05";
+        String lGroup = "L04";
+        String pGroup = "P02";
+
+//        when
+        Set<Exam> exams = examRepository.findExamsByGroupsIdentifier(generalGroup, kGroup, lGroup, pGroup);
+        List<String> examsTitles = exams.stream().map(Exam::getTitle).toList();
+
+//        then
+        assertEquals(3, exams.size());
+        assertTrue(examsTitles.contains("Exam 1"));
+        assertTrue(examsTitles.contains("Exam 2"));
+        assertTrue(examsTitles.contains("Exam 3"));
+    }
+
+    /**
+     * test if method return empty set identifiers don't match
+     */
+    @Test
+    void testNothingMatch() {
+//        given
+        Exam exam1 = Exam.builder()
+                .title("Exam 1")
+                .description("Exam 1")
+                .date(LocalDateTime.now().plusDays(1))
+                .examGroups("12K2, K01,")
+                .examType(examType)
+                .build();
+        examRepository.save(exam1);
+        Exam exam2 = Exam.builder()
+                .title("Exam 2")
+                .description("Exam 2")
+                .date(LocalDateTime.now().plusDays(1))
+                .examGroups("12K3, L05")
+                .examType(examType)
+                .build();
+        examRepository.save(exam2);
+
+        String generalGroup = "14K3";
+        String kGroup = "K05";
+        String lGroup = "L02";
+        String pGroup = "P02";
+
+//        when
+        Set<Exam> exams = examRepository.findExamsByGroupsIdentifier(generalGroup, kGroup, lGroup, pGroup);
+
+//        then
+        assertTrue(exams.isEmpty());
+    }
+}
