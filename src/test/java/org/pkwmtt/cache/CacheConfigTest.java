b05eb7b0e0093b64f3e91d3441c6e5d7f59f9278
Mikołaj Florczak <84631301+florczaq@users.noreply.github.com>
2025-08-08 20:39:26 +0200

    Fix: Cast data to json in cache for better storage

--- src/test/java/org/pkwmtt/cache/CacheConfigTest.java
+++ src/test/java/org/pkwmtt/cache/CacheConfigTest.java
@@ -2,14 +2,10 @@ package org.pkwmtt.cache;
 
 import org.junit.jupiter.api.Test;
 import org.pkwmtt.timetable.TimetableCacheService;
-import org.pkwmtt.timetable.dto.TimetableDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
