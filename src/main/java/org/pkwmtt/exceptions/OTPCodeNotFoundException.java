127f698c104a8beb2ee9fe805e924d0388c94cb1
Mikołaj Florczak <84631301+florczaq@users.noreply.github.com>
2025-08-23 22:24:20 +0200

    Authenticate and validate otp code service

--- src/main/java/org/pkwmtt/exceptions/OTPCodeNotFoundException.java
+++ src/main/java/org/pkwmtt/exceptions/OTPCodeNotFoundException.java
@@ -1,7 +1,5 @@
 package org.pkwmtt.exceptions;
 
-import org.springframework.data.crossstore.ChangeSetPersister;
-
 public class OTPCodeNotFoundException
   extends IllegalArgumentException {
     public OTPCodeNotFoundException () {
--- src/main/java/org/pkwmtt/otp/repository/OTPCodeRepository.java
+++ src/main/java/org/pkwmtt/otp/repository/OTPCodeRepository.java
@@ -3,7 +3,6 @@ package org.pkwmtt.otp.repository;
 import org.pkwmtt.examCalendar.entity.OTPCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface OTPCodeRepository
