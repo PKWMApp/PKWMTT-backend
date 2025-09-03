85ba074dd4d6c6e661791b3ca3a6716681970610
Mikołaj Florczak <84631301+florczaq@users.noreply.github.com>
2025-09-03 14:00:04 +0200

    Revert "Merge remote-tracking branch 'origin/main' into examCalendar"
    This reverts commit 9f6a895f179fa3f92a9a03e48548ae6d2c4aec68, reversing
    changes made to f481029273851772e2466ad3374d13d053a2b45b.

--- src/main/java/org/pkwmtt/security/config/SpringSecurity.java
+++ src/main/java/org/pkwmtt/security/config/SpringSecurity.java
@@ -1,17 +1,12 @@
 package org.pkwmtt.security.config;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.pkwmtt.security.auth.provider.OTPAuthenticationProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.List;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
@@ -19,10 +14,7 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 //@EnableWebSecurity
 @Slf4j
 @Configuration
-@RequiredArgsConstructor
 public class SpringSecurity {
-
-    private final OTPAuthenticationProvider otpAuthenticationProvider;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -37,10 +29,5 @@ public class SpringSecurity {
             .sessionManagement(session -> session.sessionCreationPolicy(STATELESS));
         log.info("Configuring Success...");
         return http.build();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager() {
-        return new ProviderManager(List.of(otpAuthenticationProvider));
     }
 }
--- src/main/java/org/pkwmtt/security/token/JwtService.java
+++
@@ -1,9 +0,0 @@
-package org.pkwmtt.security.token;
-
-import org.pkwmtt.security.token.dto.UserDTO;
-
-public interface JwtService {
-    String generateToken(UserDTO user);
-    Boolean validateToken(String token);
-    String getUserIdFromToken(String token);
-}
