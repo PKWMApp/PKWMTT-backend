814e766198ab98a792674b3ef6462be13112215b
PatMaz999 <Patryk.m.3456@gmail.com>
2025-09-25 20:14:23 +0200

    update database

--- init.sql
+++ init.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: Wrz 24, 2025 at 02:41 PM
+-- Generation Time: Wrz 25, 2025 at 06:13 PM
 -- Wersja serwera: 9.3.0
 -- Wersja PHP: 8.2.27
 
@@ -36,14 +36,15 @@ CREATE TABLE IF NOT EXISTS `admin_keys` (
   `description` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`key_id`),
   UNIQUE KEY `unique_value` (`value`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Zrzut danych tabeli `admin_keys`
 --
 
 INSERT INTO `admin_keys` (`key_id`, `value`, `description`) VALUES
-(3, '0923cd6f-cd33-4883-87e4-ae3b50b80a3f', 'mikolaj');
+(3, '0923cd6f-cd33-4883-87e4-ae3b50b80a3f', 'mikolaj'),
+(4, '2868b02b-a5dd-4386-a723-e450e5f54418', 'desc');
 
 -- --------------------------------------------------------
 
@@ -82,19 +83,21 @@ CREATE TABLE IF NOT EXISTS `exams` (
   `exam_type_id` int NOT NULL,
   PRIMARY KEY (`exam_id`),
   KEY `exam_type_id_idx` (`exam_type_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Zrzut danych tabeli `exams`
 --
 
 INSERT INTO `exams` (`exam_id`, `title`, `description`, `exam_date`, `exam_type_id`) VALUES
-(1, 'Kolokwium z matematyki', 'Pierwsze kolokwium obejmujące rozdziały 1–3', '2025-10-01 10:00:00', 1),
 (2, 'Egzamin końcowy z programowania', 'Egzamin pisemny i praktyczny', '2025-01-20 09:00:00', 2),
 (3, 'Projekt z baz danych', 'Oddanie projektu grupowego', '2025-06-15 23:59:00', 3),
 (4, 'Kolokwium z fizyki', 'Druga część materiału: mechanika', '2025-11-05 12:00:00', 1),
 (5, 'Egzamin końcowy z ekonomii', 'Egzamin pisemny testowy', '2025-02-10 08:30:00', 2),
-(6, 'Projekt z systemów operacyjnych', 'Prezentacja projektu semestralnego', '2025-06-25 14:00:00', 3);
+(6, 'Projekt z systemów operacyjnych', 'Prezentacja projektu semestralnego', '2025-06-25 14:00:00', 3),
+(7, 'test authorities', 'do usuniecia', '2027-09-01 09:00:00', 3),
+(8, 'test authorities', 'do usuniecia', '2027-09-01 09:00:00', 3),
+(9, 'test authorities', 'do usunieciaaaaa', '2027-09-01 09:00:00', 3);
 
 -- --------------------------------------------------------
 
@@ -110,15 +113,13 @@ CREATE TABLE IF NOT EXISTS `exams_groups` (
   PRIMARY KEY (`exam_group_id`),
   KEY `exam_id_idx` (`exam_id`),
   KEY `group_id_idx` (`group_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Zrzut danych tabeli `exams_groups`
 --
 
 INSERT INTO `exams_groups` (`exam_group_id`, `exam_id`, `group_id`) VALUES
-(7, 1, 9),
-(8, 1, 10),
 (9, 2, 12),
 (10, 2, 13),
 (11, 2, 14),
@@ -130,7 +131,11 @@ INSERT INTO `exams_groups` (`exam_group_id`, `exam_id`, `group_id`) VALUES
 (17, 5, 12),
 (18, 5, 13),
 (19, 6, 15),
-(20, 6, 16);
+(20, 6, 16),
+(21, 7, 21),
+(22, 7, 22),
+(23, 8, 9),
+(24, 9, 9);
 
 -- --------------------------------------------------------
 
@@ -165,7 +170,7 @@ CREATE TABLE IF NOT EXISTS `general_group` (
   `general_group_id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`general_group_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Zrzut danych tabeli `general_group`
@@ -175,7 +180,9 @@ INSERT INTO `general_group` (`general_group_id`, `name`) VALUES
 (17, '11A'),
 (18, '12E'),
 (19, '13K'),
-(20, '14M');
+(20, '14M'),
+(21, '12K'),
+(22, '11K');
 
 -- --------------------------------------------------------
 
@@ -185,11 +192,21 @@ INSERT INTO `general_group` (`general_group_id`, `name`) VALUES
 
 DROP TABLE IF EXISTS `moderators`;
 CREATE TABLE IF NOT EXISTS `moderators` (
-  `moderator_id` varchar(36) NOT NULL,
+  `moderator_id` binary(16) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` varchar(50) NOT NULL,
   PRIMARY KEY (`moderator_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Zrzut danych tabeli `moderators`
+--
+
+INSERT INTO `moderators` (`moderator_id`, `password`, `role`) VALUES
+(0x10b4cd4f840445ba9fff930fda8229c7, '$2a$10$zjcQISWSqPpMWQv99XWneOaHWiqTRhiXUJq5FT8iXbET.3hZfO0GO', 'MODERATOR'),
+(0x561e6e496eab4e9e965ac6c8ffe24293, '$2a$10$puIitW1sPdjyqCs2nbpco.wRAcGOpuWiOj6iQ0siFKOBaKmIS9ghK', 'MODERATOR'),
+(0x9e39a89631924bd6a38b0ee6e56be221, '$2a$10$e6H5n6xu4NymerBHqvO42OhUVg3aOHPpCPo0.TSDH1b/graC5FomC', 'MODERATOR'),
+(0xd45dd77e68ac45908a12340b35d04b6c, '$2a$10$k6aoa0OU8RKbCA4WHu4yDuMOZFmxP2zeX7Cjw3GmLVml2dDm6QGEG', 'MODERATOR');
 
 -- --------------------------------------------------------
 
@@ -212,7 +229,6 @@ CREATE TABLE IF NOT EXISTS `otp_codes` (
 --
 
 INSERT INTO `otp_codes` (`otp_code_id`, `code`, `expire`, `general_group_id`) VALUES
-(1, 'ABC123', '2025-08-18 19:51:40', 17),
 (2, 'XYZ789', '2025-08-18 20:51:40', 18),
 (3, 'QWE456', '2025-08-18 21:51:40', 19),
 (4, 'JKL999', '2025-08-18 22:51:40', 20);
@@ -229,13 +245,14 @@ CREATE TABLE IF NOT EXISTS `student_groups` (
   `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`group_id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Zrzut danych tabeli `student_groups`
 --
 
 INSERT INTO `student_groups` (`group_id`, `name`) VALUES
+(22, '11A'),
 (9, '11A1'),
 (10, '11A2'),
 (12, '12E1'),
@@ -244,7 +261,8 @@ INSERT INTO `student_groups` (`group_id`, `name`) VALUES
 (15, '13K1'),
 (16, '13K2'),
 (17, '13K3'),
-(18, '14M1');
+(18, '14M1'),
+(21, 'P01');
 
 -- --------------------------------------------------------
 
@@ -261,7 +279,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `role` enum('ADMIN','REPRESENTATIVE') COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'REPRESENTATIVE',
   PRIMARY KEY (`user_id`),
   KEY `general_group_id_idx` (`general_group_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Zrzut danych tabeli `users`
@@ -271,7 +289,13 @@ INSERT INTO `users` (`user_id`, `general_group_id`, `email`, `is_active`, `role`
 (1, 17, 'user11a@example.com', 1, 'REPRESENTATIVE'),
 (2, 18, 'user12e@example.com', 1, 'REPRESENTATIVE'),
 (3, 19, 'user13k@example.com', 1, 'REPRESENTATIVE'),
-(4, 20, 'user14m@example.com', 1, 'ADMIN');
+(4, 20, 'user14m@example.com', 1, 'ADMIN'),
+(5, 21, 'email@ex.com', 0, 'REPRESENTATIVE'),
+(6, 21, 'email@ex.com', 0, 'REPRESENTATIVE'),
+(7, 21, 'email@ex.com', 0, 'REPRESENTATIVE'),
+(8, 21, 'email@ex.com', 0, 'REPRESENTATIVE'),
+(9, 21, 'email@ex.com', 0, 'REPRESENTATIVE'),
+(10, 22, 'email@ex.com', 0, 'REPRESENTATIVE');
 
 --
 -- Ograniczenia dla zrzutów tabel
