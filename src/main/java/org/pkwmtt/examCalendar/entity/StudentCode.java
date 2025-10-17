7871fc538193627ce54e61b031af6e9d5a1c5c48
Mikołaj Florczak <84631301+florczaq@users.noreply.github.com>
2025-10-17 20:59:20 +0200

    Refactor StudentCode class: add default values for usage and usageLimit in constructor

--- src/main/java/org/pkwmtt/examCalendar/entity/StudentCode.java
+++ src/main/java/org/pkwmtt/examCalendar/entity/StudentCode.java
@@ -19,26 +19,29 @@ public class StudentCode {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "student_code_id")
     private Integer studentCodeId;
-
+    
     @Column(nullable = false)
     private String code;
-
+    
     @Column(nullable = false)
     private LocalDateTime expire;
-
+    
     @ManyToOne
     @JoinColumn(name = "superior_group_id", nullable = false)
     private SuperiorGroup superiorGroup;
-
+    
     @Column(name = "usage")
     private Integer usage;
-
+    
     @Column(name = "usage_limit")
     private Integer usageLimit;
-
-    public StudentCode(String code, SuperiorGroup superiorGroup) {
+    
+    
+    public StudentCode (String code, SuperiorGroup superiorGroup) {
         this.code = code;
         this.superiorGroup = superiorGroup;
         this.expire = LocalDateTime.now().plusDays(1);
+        this.usage = 0;
+        this.usageLimit = 99;
     }
 }
