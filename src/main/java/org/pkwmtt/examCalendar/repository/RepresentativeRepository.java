bec78935b3be7de9640d6e72db175a19e88a256f
Mikołaj Florczak <84631301+florczaq@users.noreply.github.com>
2025-10-17 14:03:21 +0200

    Delete redundant query

--- src/main/java/org/pkwmtt/examCalendar/repository/RepresentativeRepository.java
+++ src/main/java/org/pkwmtt/examCalendar/repository/RepresentativeRepository.java
@@ -1,19 +1,20 @@
 package org.pkwmtt.examCalendar.repository;
 
 import jakarta.transaction.Transactional;
-import org.pkwmtt.examCalendar.entity.SuperiorGroup;
 import org.pkwmtt.examCalendar.entity.Representative;
+import org.pkwmtt.examCalendar.entity.SuperiorGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Modifying;
 
 import java.util.Optional;
 
 public interface RepresentativeRepository extends JpaRepository<Representative, Integer> {
-    Optional<Representative> findByEmail(String email);
-    Optional<Representative> findBySuperiorGroup(SuperiorGroup superiorGroup);
-
-    @Query("SELECT g.name FROM Representative r LEFT JOIN r.superiorGroup g where r.email = :email")
+    Optional<Representative> findByEmail (String email);
+    
+    Optional<Representative> findBySuperiorGroup (SuperiorGroup superiorGroup);
+    
+    @Modifying
     @Transactional
-    void deleteRepresentativeByEmail(String email);
+    void deleteRepresentativeByEmail (String email);
 }
 
