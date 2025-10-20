380d15e44eeb59556204a3fcb86852f4adc25b13
Mikołaj Florczak <84631301+florczaq@users.noreply.github.com>
2025-10-20 19:38:00 +0200

    Add BugReport entity, DTO, repository, mapper, and service for bug report management

---
+++ src/main/java/org/pkwmtt/reports/dto/BugReportDTO.java
@@ -0,0 +1,15 @@
+package org.pkwmtt.reports.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@Getter
+public class BugReportDTO {
+    String userGroups;
+    String description;
+    Date IssuedAt;
+}
---
+++ src/main/java/org/pkwmtt/reports/mapper/BugReportsMapper.java
@@ -0,0 +1,33 @@
+package org.pkwmtt.reports.mapper;
+
+import org.pkwmtt.reports.dto.BugReportDTO;
+import org.pkwmtt.reports.entities.BugReport;
+
+public class BugReportsMapper {
+    private BugReportsMapper () {
+    }
+    
+    
+    public static BugReportDTO toDto (BugReport src) {
+        if (src == null) {
+            return null;
+        }
+        
+        return new BugReportDTO(
+          src.getUserGroups(),
+          src.getDescription(),
+          src.getIssuedAt()
+        );
+    }
+    
+    public static BugReport toEntity (BugReportDTO dto) {
+        if (dto == null) {
+            return null;
+        }
+        return new BugReport(
+          dto.getUserGroups(),
+          dto.getDescription(),
+          dto.getIssuedAt()
+        );
+    }
+}
